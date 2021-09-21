55c3ed12a8aa6af03f61895d5bc53ce8b8d2d062
0xmikemikemike <90872644+0xmikemikemike@users.noreply.github.com>
2021-09-21 13:11:53 -0400

    Add files via upload

---
+++ dictLines_poppushchars.py
@@ -0,0 +1,24 @@
+import wordninja
+import base64
+import binascii
+
+file1 = open('decodedLines3.txt', 'r')
+Lines = file1.readlines()
+
+count = 0
+
+#lm = wordninja.LanguageModel('common_words.txt.gz')
+# Strips the newline character
+for line in Lines:
+    count += 1
+    
+    string_to_test = line.strip().lower()
+    
+    for x in range(32):
+        string_to_test = string_to_test[1:] + string_to_test[0]
+        
+        split_string = wordninja.split(string_to_test)
+
+        if len(split_string ) < 12:
+            print ("line: " + str(count) + "shift: " + str(x) + " split: ")
+            print (split_string)
